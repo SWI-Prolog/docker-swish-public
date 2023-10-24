1970c5bafd10e78dbb73a07c3749b57acff68506
Jan Wielemaker <J.Wielemaker@vu.nl>
2023-10-24 15:44:54 +0200

    Make swish.sh more flexible.

--- swish.sh
+++ swish.sh
@@ -1,14 +1,33 @@
 #!/bin/bash
 
 port=3050
+sshport=3250
 data="$(pwd)"
 dopts=
+sopts=
 fake=no
+IMG=${IMG-swish}
+name=
+RESTART="--restart unless-stopped"
+
+config-only()
+{ port=0
+  sshport=0
+  RESTART=--rm
+  dopts+=" -it"
+}
+
 
 done=no
 while [ $done = no ]; do
   case "$1" in
     --port=*)	port="$(echo $1 | sed 's/[^=]*=//')"
+		shift
+		;;
+    --name=*)	name="$(echo $1 | sed 's/[^=]*=//')"
+		shift
+		;;
+    --ssh=*)	sshport="$(echo $1 | sed 's/[^=]*=//')"
 		shift
 		;;
     --data=*)	data="$(echo $1 | sed 's/[^=]*=//')"
@@ -30,13 +49,38 @@ while [ $done = no ]; do
     -it)	dopts="$dopts -it"
 		shift
 		;;
-    *)		done=yes
+    --rm)	RESTART=--rm
+		shift
+		;;
+    --help|--auth*|--add-user|--add-config|--list-config)
+	        sopts+=" $1"
+		config-only
+		shift
+		;;
+    *)		sopts+=" $*"
+		done=yes
 		;;
   esac
 done
 
-if [ $fake = no ]; then
-  docker run -p $port:3050 -p 3250:3250 -v "$data":/data $dopts swish $*
+if [ "$sshport" != 0 ]; then
+    SSHMAP="-p 127.0.0.1:$sshport:3250"
 else
-  echo docker run -d --restart unless-stopped -p 127.0.0.1:$port:3050 -p 127.0.0.1:3250:3250 -v "$data":/data $dopts swish $*
+    SSHMAP=
+fi
+
+if [ "$port" != 0 ]; then
+    HTTPMAP="-p 127.0.0.1:$port:3050"
+else
+    HTTPMAP=
+fi
+
+if [ ! -z "$name" ]; then
+    dopts+=" --name=$name"
+fi
+
+if [ $fake = no ]; then
+  docker run $RESTART $HTTPMAP $SSHMAP -v "$data":/data $dopts $IMG $sopts
+else
+  echo docker run $RESTART $HTTPMAP $SSHMAP -v "$data":/data $dopts $IMG $sopts
 fi
